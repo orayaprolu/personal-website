d0ebcd22d1685f7d983d5babe0ddbeaef119ccbb
orayaprolu <orayaprolu@gmail.com>
2023-08-13 14:25:28 -0700

    Did most of CSS for NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,17 @@
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import {useState, useEffect} from 'react';
 
-export const Navbar = () => {
+import logo from '../assets/images/logo.svg';
+import navIcon1 from '../assets/images/nav-icon1.svg';
+import navIcon2 from '../assets/images/nav-icon2.svg';
+import navIcon3 from '../assets/images/nav-icon3.svg';
+
+export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
-    const [srcolled, setScrolled] = useState(false)
+    const [scrolled, setScrolled] = useState(false)
 
     useEffect(() => {
         const onScroll = () => {
@@ -25,7 +30,7 @@ export const Navbar = () => {
         <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
         <Container>
             <Navbar.Brand href="#home">
-                <img src={''} alt = "Logo" />
+                <img src={logo} alt = "Logo" />
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav">
                 <span className="navbar-toggler-icon"></span>
@@ -38,9 +43,9 @@ export const Navbar = () => {
             </Nav>
             <span className='navbar-text'>
                 <div className='social-icon'>
-                    <a href='#'><img src={} alt = "" /></a>
-                    <a href='#'><img src={} alt = "" /></a>
-                    <a href='#'><img src={} alt = "" /></a>
+                    <a href='#'><img src={navIcon1} alt = "" /></a>
+                    <a href='#'><img src={navIcon2} alt = "" /></a>
+                    <a href='#'><img src={navIcon3} alt = "" /></a>
                 </div>
                 <button className='vvd' onClick={() => console.log('connect')}/><span>Let's Connect!</span>
             </span>
